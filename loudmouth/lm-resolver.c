821904820cbc461597d742c7b9578971b12dbaa8
Mikael Hallendal <micke@imendio.com>
2008-10-22 14:08:45 +0200

    Made type property on LmResolver settable after creation.
    Currently the type of the resolver is changed after doing the initial SRV lookup so the property can't be creation only.

--- loudmouth/lm-resolver.c
+++ loudmouth/lm-resolver.c
@@ -108,7 +108,7 @@ lm_resolver_class_init (LmResolverClass *class)
                                                        LM_RESOLVER_HOST,
                                                        LM_RESOLVER_SRV,
                                                        LM_RESOLVER_HOST,
-                                                       G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
+                                                       G_PARAM_READWRITE));
 
     g_object_class_install_property (object_class,
                                      PROP_HOST,
