3a1fb75ba0b1239cf219241ebafee33c755568ea
Myhailo Danylenko <isbear@ukrpost.net>
2012-11-15 21:03:52 +0200

    [autogen] always fail, if not in source root

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,12 @@
 : ${AUTORECONF=autoreconf}
 : ${GTKDOCIZE=gtkdocize}
 
+if ! [ -f "./autogen.sh" ]; then
+	# Avoid creation of files in random directories
+	echo "Error: You have to run this script in the root of loudmouth sources."
+	exit 1
+fi
+
 if [ "x$1" = "x-h" ] || [ "x$1" = "x--help" ]; then
 	echo "./autogen.sh [-g]"
 	echo
@@ -15,19 +21,13 @@ elif [ "x$1" = "x-n" ]; then
 	# ensure, that gtk-doc.make exists and is readable
 	# otherwise automake will fail - it is included from
 	# doc/reference/Makefile.am
-	if [ -f "./autogen.sh" ]; then
-		if [ -h "./gtk-doc.make" ]; then
-			rm -f "./gtk-doc.make"
-		fi
-		if [ -n "./gtk-doc.make" ]; then
-			: > "./gtk-doc.make"
-		fi
-		GTKDOCIZE=":"
-	else
-		# Avoid creation of files in random directories
-		echo "Error: You have to run this script from the root of loudmouth sources."
-		exit 1
+	if [ -h "./gtk-doc.make" ]; then
+		rm -f "./gtk-doc.make"
 	fi
+	if [ -n "./gtk-doc.make" ]; then
+		: > "./gtk-doc.make"
+	fi
+	GTKDOCIZE=":"
 fi
 
 $GTKDOCIZE || exit 1
